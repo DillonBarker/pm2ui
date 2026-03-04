238bfaf72dbf7edffeda1d261951d000c43a0199
Dillon Barker <barker.dill98@gmail.com>
2026-03-04 09:12:28 +0000

    go releaser and version command

--- cmd/root.go
+++ cmd/root.go
@@ -7,8 +7,17 @@ import (
 	"github.com/DillonBarker/pm2ui/internal/view"
 )
 
+var Version = "dev"
+
 // Execute is the entry point for the application.
 func Execute() {
+	for _, arg := range os.Args[1:] {
+		if arg == "--version" || arg == "-v" || arg == "-version" {
+			fmt.Printf("pm2ui %s\n", Version)
+			return
+		}
+	}
+
 	layout := view.NewLayout()
 	if err := layout.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
